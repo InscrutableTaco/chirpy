69fb4a8c1dbaf9aecc55869c39df584fa2e7db7e
Jonathan Gibson <jonathan.w.d.gibson@gmail.com>
2025-10-04 10:38:23 -0700

    worked on json handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"      // logging errors and info
 	"net/http" // web server and HTTP utilities
 )
@@ -11,11 +12,35 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))                                       // write response body
 }
 
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct {
+		// these tags indicate how the keys in the JSON should be mapped to the struct fields
+		// the struct fields must be exported (start with a capital letter) if you want them parsed
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	// params is a struct with data populated successfully
+	// ...
+
+}
+
 func routes(cfg *apiConfig) http.Handler {
 	mux := http.NewServeMux()                          // create a new router
 	mux.HandleFunc("GET /api/healthz", healthzHandler) // register handler for /healthz
 	mux.HandleFunc("GET /admin/metrics", cfg.writeNumberOfRequests)
 	mux.HandleFunc("POST /admin/reset", cfg.resetHitsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateHandler)
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))) // register file server for /app/
 	mux.Handle("/app", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))  // and for just /app because why not
 	return mux                                                                                              // return the router
