43cf132998e07206f9cf542d1bce919073bd58ff
Jonathan Gibson <jonathan.w.d.gibson@gmail.com>
2025-10-18 17:16:05 -0700

    more jwt

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -26,10 +26,12 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
 		Issuer:    "chirpy",
-		IssuedAt:  jwt.NewNumericDate(time.Now()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
 		Subject:   userID.String(),
 	})
 
-	jwt, err := token.SignedString()
+	signed, err := token.SignedString([]byte(tokenSecret))
+
+	return signed, err
 }
